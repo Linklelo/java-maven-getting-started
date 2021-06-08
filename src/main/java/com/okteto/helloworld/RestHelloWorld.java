135266c65a0a7c4f84565a91086c54de00cc4807
Linkle <1096056364@qq.com>
2021-06-08 20:11:12 +0800

    Update RestHelloWorld.java

--- src/main/java/com/okteto/helloworld/RestHelloWorld.java
+++ src/main/java/com/okteto/helloworld/RestHelloWorld.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RestHelloWorld {
 	
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello world!";
+		return "[{\"temp\":\"20℃/30℃\",\"weather\":\"晴天多云\",\"name\":\"北京\",\"pm\":\"80\",\"wind\":\"1级\"},\n" +
+                "  {\"temp\":\"15℃/24℃\",\"weather\":\"晴天\",\"name\":\"成都\",\"pm\":\"98\",\"wind\":\"3级\"},\n" +
+                "  {\"temp\":\"15℃/24℃\",\"weather\":\"晴天\",\"name\":\"上海\",\"pm\":\"98\",\"wind\":\"3级\"},\n" +
+                "  {\"temp\":\"26℃/32℃\",\"weather\":\"多云\",\"name\":\"广州\",\"pm\":\"30\",\"wind\":\"2级\"}]";
 	}
 }
